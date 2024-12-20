a9c4deba95ed2645437e8300f439f7284f2ed94e
Khyojun <nandong5923@naver.com>
2024-12-20 18:35:29 +0900

    feat : first commit

---
+++ src/test/java/com/food/ordering/system/practicestackorder/PracticeStackOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.practicestackorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeStackOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
