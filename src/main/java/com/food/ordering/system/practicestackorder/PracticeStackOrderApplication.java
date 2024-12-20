224bb1025f482246e8221831fdbbf65cdb490f94
Khyojun <nandong5923@naver.com>
2024-12-20 18:37:04 +0900

    feat : 초기 application 파일 yml 파일 생성

---
+++ src/main/java/com/food/ordering/system/practicestackorder/PracticeStackOrderApplication.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.practicestackorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeStackOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeStackOrderApplication.class, args);
+    }
+
+}
